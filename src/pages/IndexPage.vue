d7eb3da81193a839f2a5eaca2bd8693bc53b24fa
Bintang Kurniawan <bintang.kurniawannn.98@gmail.com>
2024-03-26 13:41:01 +0700

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="row items-center justify-evenly text-red-500">
+    <p class="text-red-500">kknjt</p>
+  </q-page>
+</template>
+<script lang="ts">
+import { api } from 'src/boot/axios';
+
+export default {
+  mounted() {
+    this.getData();
+  },
+
+  methods: {
+    async getData() {
+      await api
+        .get('/list?filter=manga')
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err));
+    },
+  },
+};
+</script>
